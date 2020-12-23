d1c727b8cb3bb71d0cc15f22875cc5a3867a605a
jw20191n <clairejw1015@gmail.com>
2020-12-22 20:53:47 -0500

    added several dev dependecies

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter, NavLink } from 'react-router-dom';
 import { Menu } from 'antd';
 import Router from './router';
-
-import 'antd/dist/antd.css';
+import './App.less';
+// import 'antd/dist/antd.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Menu theme="dark">
+      <Menu className="menu" theme="dark">
         <Menu.Item><NavLink to="/">Home</NavLink></Menu.Item>
         <Menu.Item><NavLink to="/login">Login</NavLink></Menu.Item>
         <Menu.Item><NavLink to="/register">Register</NavLink></Menu.Item>
