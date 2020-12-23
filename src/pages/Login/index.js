e658033f1d82ab0a34e62e9815b1945f6b04fc2e
jw20191n <clairejw1015@gmail.com>
2020-12-22 21:13:22 -0500

    added login tabs

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Tabs } from 'antd';
+import styles from './index.module.less';
+
+const { TabPane } = Tabs;
 
 const Login = () => {
     return(
-        <div>
-            Login
+        <div className={styles.loginContainer}>
+            {/* set the defaultActiveKey as 1, so the tab will be highlighted by default */}
+            <Tabs defaultActiveKey="1">
+                <TabPane tab="username and password login" key="1">
+                    adsads
+                </TabPane>
+                <TabPane tab="phone number login" key="2">
+                    dsads
+                </TabPane>
+            </Tabs>
         </div>
     )
 };
