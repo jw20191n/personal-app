71f1370678150d37e3bf36ef6eed2d2e9a2c6d8c
jw20191n <clairejw1015@gmail.com>
2020-12-22 20:05:54 -0500

    imported ant design

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, NavLink } from 'react-router-dom';
+import { Menu } from 'antd';
 import Router from './router';
 
+import 'antd/dist/antd.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/login">Login</a></li>
-        <li><a href="/register">Register</a></li>
-      </ul>
+      <Menu theme="dark">
+        <Menu.Item><NavLink to="/">Home</NavLink></Menu.Item>
+        <Menu.Item><NavLink to="/login">Login</NavLink></Menu.Item>
+        <Menu.Item><NavLink to="/register">Register</NavLink></Menu.Item>
+      </Menu>
 
       <Router></Router>
     </BrowserRouter>
