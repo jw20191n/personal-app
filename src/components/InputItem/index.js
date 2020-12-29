c71c2a40eaf700fb33c7b435c391f6663dcb162d
jw20191n <clairejw1015@gmail.com>
2020-12-29 17:01:55 -0500

    changed the way of sending props to Input component

--- src/components/InputItem/index.js
+++ src/components/InputItem/index.js
@@ -2,9 +2,9 @@ import { Input } from 'antd';
 import React from 'react';
 
 const InputItem = (props) => {
-    const { placeholder, size } = props;
+    
     return(
-        <Input placeholder={placeholder} size={size}/>
+        <Input {...props}/>
     )
 }
 
