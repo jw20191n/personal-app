47aa4e064e0128b4c33a655cd925cd84bec0e83b
jw20191n <clairejw1015@gmail.com>
2020-12-29 20:27:09 -0500

    store tablist array as a separate vairable operationTabList

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,31 @@ import React from 'react';
 import { Row, Col, Card } from 'antd';
 import styles from './index.module.less';
 
+const operationTabList = [{ 
+    key: 'article',
+    tab: (
+        <span>
+            Article <span>(8)</span>
+        </span>
+    )
+},
+{ 
+    key: 'applications',
+    tab: (
+        <span>
+            App <span>(18)</span>
+        </span>
+    )
+},
+{ 
+    key: 'projects',
+    tab: (
+        <span>
+            Project <span>(10)</span>
+        </span>
+    )
+}];
+
 const Home = () => {
     return(
         <div className={styles.container}>
@@ -14,30 +39,7 @@ const Home = () => {
                 <Col lg={17} md={24}>
                     <Card 
                         bordered={false}
-                        tabList={[ { 
-                            key: 'article',
-                            tab: (
-                                <span>
-                                    Article <span>(8)</span>
-                                </span>
-                            )
-                        },
-                        { 
-                            key: 'applications',
-                            tab: (
-                                <span>
-                                    App <span>(18)</span>
-                                </span>
-                            )
-                        },
-                        { 
-                            key: 'projects',
-                            tab: (
-                                <span>
-                                    Project <span>(10)</span>
-                                </span>
-                            )
-                        } ]}
+                        tabList={operationTabList}
                     >
                         pqstr
                     </Card>
