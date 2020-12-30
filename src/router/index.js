0d1b8387cc8c122e0a6a6c18e4b3b658f496630d
jw20191n <clairejw1015@gmail.com>
2020-12-29 19:55:43 -0500

    router convert to lazy mode and suspense fallback is a string loading...

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,22 @@
 import { Route, Switch } from 'react-router-dom';
-import React from 'react';
-import Home from '../pages/Home';
-import Login from '../pages/Login';
-import Register from '../pages/Register';
+import React, { lazy, Suspense } from 'react';
+// import Home from '../pages/Home';
+// import Login from '../pages/Login';
+// import Register from '../pages/Register';
 
+const Home = lazy( () => import('../pages/Home'));
+const Login = lazy( () => import('../pages/Login'));
+const Register = lazy( () => import('../pages/Register'));
 
-
-const Router = () => {
-    return(
+const Router = () => (
+    <Suspense fallback="loading...">
         <Switch>
             <Route exact path="/" component={Home}></Route>
             <Route exact path="/login" component={Login}></Route>
             <Route exact path="/register" component={Register}></Route>
         </Switch>
-    ) 
-}
+    </Suspense>
+)
+   
 
 export default Router;
