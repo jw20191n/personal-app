ffbf45ff2df35e2f9d7a54645f22010d248cd626
jw20191n <clairejw1015@gmail.com>
2021-01-02 15:51:10 -0500

    finished adding a new tag feature

--- src/pages/Home/components/TagList/index.js
+++ src/pages/Home/components/TagList/index.js
@@ -6,15 +6,35 @@ import styles from './index.module.less';
 const TagList = ({ tags }) => {
 
     const [inputVisible, setInputVisible] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+    const [newTags, setNewTags] = useState([]);
+
     const showInput = () => {
         setInputVisible(true);
+    }
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    }
+
+    const handleInputConfirm = () => {
+        let tempTags = [...newTags]; 
+        if (inputValue && !tags.concat(tempTags).map(({label}) => label).includes(inputValue)) {
+            tempTags = [...tempTags, 
+                {  
+                    key: `new-${tempTags.length}`, 
+                    label: inputValue 
+                }];
+            setNewTags(tempTags);
+            setInputVisible(false);
+            setInputValue('');
+        }
     }
 
     return (
         <div className={styles.tags}>
            <div className={styles.tagsTitle}>Tag</div> 
            {
-               (tags || []).map((item)=> (
+               (tags || []).concat(newTags).map((item)=> (
                     <Tag key={item.key}>{item.label}</Tag>
                ))
            }
@@ -23,6 +43,10 @@ const TagList = ({ tags }) => {
                <Input 
                     size="small"
                     style={{ width: 78 }}
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    onBlur={handleInputConfirm}
+                    onPressEnter={handleInputConfirm}
                 />
            }
 
