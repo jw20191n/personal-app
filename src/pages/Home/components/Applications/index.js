9d173ebd14c2431b0fe6c95fd4987a54aeba55d5
jw20191n <clairejw1015@gmail.com>
2021-01-05 20:37:28 -0500

    added card info to load the number of active and new users

--- src/pages/Home/components/Applications/index.js
+++ src/pages/Home/components/Applications/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { List, Card, Tooltip, Dropdown, Menu } from 'antd';
+import { List, Card, Tooltip, Dropdown, Menu, Avatar } from 'antd';
 import { DownloadOutlined, EditOutlined, ShareAltOutlined, EllipsisOutlined } from '@ant-design/icons';
+import numeral from 'numeral';
 import styles from './index.module.less';
 
 const itemMenu = (
@@ -22,6 +23,42 @@ const itemMenu = (
         </Menu.Item>
     </Menu>
 )
+
+const CardInfo = ({activeUser, newUser}) => (
+    <div className={styles.cardInfo}>
+        <div>
+            <p>Active User</p>
+            <p>{activeUser}</p>
+        </div>
+
+        <div>
+            <p>New User</p>
+            <p>{newUser}</p>
+        </div>
+    </div>
+)
+    
+function formatMil(val) {
+    const v = val * 1;
+    if(!v || Number.isNaN(v)) return '';
+    let result = val;
+    if(val > 1000) {
+        result = (
+            <span>
+                {Math.floor(val/1000)}
+                <span>K</span>
+            </span>
+        )
+    }else if (val > 1000000){
+        result = (
+            <span>
+                {Math.floor(val/1000000)}
+                <span>M</span>
+            </span>
+        )
+    }
+    return result;
+}
 
 const Applications = ({list}) => {
     return(
@@ -52,7 +89,16 @@ const Applications = ({list}) => {
                             </Dropdown>
                         ]} 
                     >
-                        dsadsa
+                        <Card.Meta 
+                            avatar={<Avatar size="small" src={item.avatar}/>}
+                            title={item.title} 
+                        />
+                        <div>
+                            <CardInfo 
+                                activeUser={formatMil(item.activeUser)}
+                                newUser={numeral(item.newUser).format('0,0')}
+                            />
+                        </div>
                     </Card>
                 </List.Item>
             )}
